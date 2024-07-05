9eefacfebeab9020901e6b8377a2f88baf0b3b4f
NataliaSokolowska <nataliasokolowska@bedeprogramistka.pl>
2024-07-05 15:21:29 +0200

    Move js files to directory Add git ignore

--- js/content.js
+++ js/content.js
@@ -1,5 +1,4 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log("Message received in content script", message); // Debug log
   if (message.action === "findEmails") {
     const emailPattern = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
     const emails = document.body.innerText.match(emailPattern) || [];
